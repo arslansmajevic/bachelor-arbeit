283782dd49c5ed489841498cf9b65569651c6ef0
Arslan Smajevic <iamarslanb@gmail.com>
2024-07-14 13:25:09 +0200

    chore: setting up project

---
+++ data-exchange-project/src/main/java/project/data_exchange_project/DataExchangeProjectApplication.java
@@ -0,0 +1,13 @@
+package project.data_exchange_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExchangeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataExchangeProjectApplication.class, args);
+	}
+
+}
---
+++ data-exchange-project/src/test/java/project/data_exchange_project/DataExchangeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.data_exchange_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExchangeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
