d63e79b35d640927895784f480da6a3c50eacf5a
Arslan Smajevic <arslan.smajevic@tuwien.ac.at>
2025-01-30 21:36:44 +0100

    docker: new database

--- data-exchange-frontend/src/app/global/globals.ts
+++ data-exchange-frontend/src/app/global/globals.ts
@@ -13,7 +13,8 @@ export class Globals {
       // assume deployed somewhere and backend is available at same host/port as frontend
       // TODO: this needs some clearing up
       // return 'http://backend-spring-boot-1:8080/api/v1';
-      return 'http://localhost:8080/api/v1';
+      console.log(window.location.port);
+      return 'http://almach.dbai.tuwien.ac.at:8080/api/v1';
       // return window.location.protocol + '//' + window.location.host + window.location.pathname + 'api/v1';
     }
   }
---
+++ graphdb-data/data/settings.js
@@ -0,0 +1,31 @@
+{
+  "properties" : {
+    "current.location" : ""
+  },
+  "import.local" : {
+    "production-repo;;sample_kg.ttl" : {
+      "name" : "sample_kg.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in 7s.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/sample_kg.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "d069a74b-2915-4785-8b2c-84e1b58182ee",
+      "timestamp" : 1738268866758,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true,
+        "contextLink" : null
+      }
+    }
+  }
+}
